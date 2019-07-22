1fcf0daa2e4d7fd3dfe8610a03cff771d0953f36
leostrupeni <leonardo.strupeni@gmail.com>
2019-07-22 13:05:54 -0300

    practica pdo 1

--- 0702 - SQL_PDO/Ejercicios.php
+++ 0702 - SQL_PDO/Ejercicios.php
@@ -4,8 +4,11 @@ require_once "../GLOBAL/config.php";
 require_once "../GLOBAL/conexion.php";
 require_once "../GLOBAL/funciones.php";
 
-$series = consultaTabla($pdo, "series");
-
+$gen=generosPelis($pdo);
+$actores=actores($pdo);
+$actorPelicula=actorPelicula($pdo);
+$episodios=episodios($pdo);
+$ratingPelis=ratingPelis($pdo);
 ?>
 
 <!DOCTYPE html>
@@ -21,19 +24,212 @@ $series = consultaTabla($pdo, "series");
 
 <body>
     <div class="container">
-        <header>
-            <h1>Listado de Series</h1>
-        </header>
+    <h1 class="text-center">Práctica I (Integradora)</h1><br>
+        <div class="row">
+            <div class="col h3">
+                1.1 - Listado Peliculas y Genero
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-6 m-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Genero</th>
+                            <th scope="col">Pelicula</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($gen as $peli) :?>
+                        <tr>
+                            <td><?=$peli['name']?></td>
+                            <td><?=$peli['title']?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
 
-        <ul>
-            <?php foreach ($series as $serie) :  ?>
-                <li>
-                    <a href="serie.php?id=<?= $serie["id"] ?>" class="text-decoration-none">
-                        <?= $serie["title"] ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <div class="row">
+            <div class="col h3">
+                1.2 - Listado Peliculas, Genero y Actores
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-10 m-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Titulo</th>
+                            <th scope="col">Genero</th>
+                            <th scope="col">Actores</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($actores as $peli) :?>
+                        <tr>
+                            <td><?=$peli['title']?></td>
+                            <td><?=$peli['name']?></td>
+                            <td><?=$peli['Nombre']?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+
+        <div class="row">
+            <div class="col h3">
+                1.3 - Listado Actores y sus peliculas
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-7 m-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Actor</th>
+                            <th scope="col">Pelicula</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($actorPelicula as $peli) :?>
+                        <tr>
+                            <td><?=$peli['Nombre']?></td>
+                            <td><?=$peli['title']?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+
+        <div class="row">
+            <div class="col h3">
+                1.5 - Episodios
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-12 m-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Temp.</th>
+                            <th scope="col">Temp. Name</th>
+                            <th scope="col">Num. Epis.</th>
+                            <th scope="col">Epis. name</th>
+                            <th scope="col">Genero</th>
+                            <th scope="col">Actores</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($episodios as $peli) :?>
+                        <tr>
+                            <td><?=$peli['NumeroTemporada']?></td>
+                            <td><?=$peli['NombreSerie']?></td>
+                            <td><?=$peli['Episodio']?></td>
+                            <td><?=$peli['NombreEpisodio']?></td>
+                            <td><?=$peli['Genero']?></td>
+                            <td><?=$peli['Cantidad_Actores']?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+
+        <div class="row">
+            <div class="col h3">
+                1.6 - Rating por Genero
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-4 m-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Genero</th>
+                            <th scope="col">Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($ratingPelis as $peli) :?>
+                        <tr>
+                            <td><?=$peli['name']?></td>
+                            <td><?=$peli['Prom_Rating']?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+        <?php $episodiosXanio=episodiosXanio($pdo,'2016') ?>
+        <div class="row">
+            <div class="col h3">
+                1.7 - Episodios 2016
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-6 m-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Serie</th>
+                            <th scope="col">Año</th>
+                            <th scope="col">Cant. 2016</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($episodiosXanio as $peli) :?>
+                        <tr>
+                            <td><?=$peli['NombreSerie']?></td>
+                            <td><?=$peli['release_date']?></td>
+                            <td><?=$peli['Cant_Episodios']?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+        
+        <?php $pAnio=2019;
+        $episodiosXanio=episodiosXanio($pdo, $pAnio) ?>
+        <div class="row">
+            <div class="col h3">
+                <?="1.8 - Episodios ". $pAnio. " (Actual)" ?>
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-6 m-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Serie</th>
+                            <th scope="col">Año</th>
+                            <th scope="col"><?="Cant. ". $pAnio ?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($episodiosXanio as $peli) :?>
+                        <tr>
+                            <td><?=$peli['NombreSerie']?></td>
+                            <td><?=$peli['release_date']?></td>
+                            <td><?=$peli['Cant_Episodios']?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+
     </div>
 
 </body>
--- GLOBAL/conexion.php
+++ GLOBAL/conexion.php
@@ -4,7 +4,7 @@ $servidor="mysql:host=".SERVIDOR.";dbname=".DB.";port=".PORT;
 
     try {
         $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
-        echo "<Script>alert('Conectado ... ')</Script>";
+        // echo "<Script>alert('Conectado ... ')</Script>";
     } catch(\Exception $e ) {   
         echo "<Script>alert('Error ... ')</Script>";exit;
     };
